e54df11366a2d35d865ec1be0734beecac420743
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2026-01-09 18:32:19 -0500

    feat(jlox): Been working on this for a minute and haven't been using git lol

---
+++ jlox/src/main/java/com/cthuloops/jlox/Interpreter.java
@@ -0,0 +1,127 @@
+package com.cthuloops.jlox;
+
+class Interpreter implements Expression.Visitor<Object> {
+
+    @Override
+    public Object visitLiteralExpression(Expression.Literal expression) {
+        return expression.value;
+    }
+
+    @Override
+    public Object visitGroupingExpression(Expression.Grouping expression) {
+        return evaluate(expression.expression);
+    }
+
+    @Override
+    public Object visitUnaryExpression(Expression.Unary expression) {
+        Object right = evaluate(expression.right);
+
+        switch (expression.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                checkNumberOperand(expression.operator, right);
+                return -(double)right;
+        }
+
+        // Unreachable.
+        return null;
+    }
+
+    private void checkNumberOperand(Token operator, Object operand) {
+        if (operand instanceof Double) {
+            return;
+        }
+        throw new RuntimeError(operator, "Operand must be a number.");
+    }
+
+    private void checkNumberOperands(Token operator,
+                                     Object left, Object right) {
+        if (left instanceof Double && right instanceof Double) {
+            return;
+        }
+
+        throw new RuntimeError(operator, "Operands must be numbers.");
+    }
+
+    @Override
+    public Object visitBinaryExpression(Expression.Binary expression) {
+        Object left = evaluate(expression.left);
+        Object right = evaluate(expression.right);
+
+        switch (expression.operator.type) {
+            case GREATER:
+                checkNumberOperands(expression.operator, left, right);
+                return (double)left > (double)right;
+
+            case GREATER_EQUAL:
+                checkNumberOperands(expression.operator, left, right);
+                return (double)left >= (double)right;
+
+            case LESS:
+                checkNumberOperands(expression.operator, left, right);
+                return (double)left < (double)right;
+
+            case LESS_EQUAL:
+                checkNumberOperands(expression.operator, left, right);
+                return (double)left <= (double)right;
+
+            case BANG_EQUAL:
+                return !isEqual(left, right);
+
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+
+            case MINUS:
+                checkNumberOperands(expression.operator, left, right);
+                return (double)left - (double)right;
+
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left + (double)right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    return (String)left + (String)right;
+                }
+                throw new RuntimeError(expression.operator,
+                     "Operands must be two numbers or two strings.");
+
+            case SLASH:
+                checkNumberOperands(expression.operator, left, right);
+                return (double)left / (double)right;
+
+            case STAR:
+                checkNumberOperands(expression.operator, left, right);
+                return (double)left * (double)right;
+        }
+
+        // Unreachable.
+        return null;
+    }
+
+    private Object evaluate(Expression expression) {
+        return expression.accept(this);
+    }
+    
+    private boolean isTruthy(Object object) {
+        if (object == null) {
+            return false;
+        }
+        if (object instanceof Boolean) {
+            return (boolean)object;
+        }
+        return true;
+    }
+
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null) {
+            return true;
+        }
+        if (a == null) {
+            return false;
+        }
+
+        return a.equals(b);
+    }
+}
---
+++ jlox/src/main/java/com/cthuloops/tool/generate_ast.py
@@ -0,0 +1,155 @@
+from dataclasses import dataclass
+from enum import StrEnum
+from os import path
+import sys
+
+class Expression_Type(StrEnum):
+    BINARY = "Binary"
+    GROUPING = "Grouping"
+    LITERAL = "Literal"
+    UNARY = "Unary"
+
+
+class Statement_Type(StrEnum):
+    ...
+
+
+class Parameter_Type(StrEnum):
+    EXPRESSION = "Expression"
+    TOKEN = "Token"
+    OBJECT = "Object"
+
+
+@dataclass(frozen=True)
+class Parameter:
+    param_type: Parameter_Type
+    param_name: str
+
+    def __str__(self):
+        return f"{self.param_type} {self.param_name}"
+
+
+@dataclass(frozen=True)
+class AST_Class:
+    base_name: str
+    name: Expression_Type | Statement_Type
+    parameters: list[Parameter]
+
+    def __str__(self):
+        return ''.join([
+            self.__generate_class_decl(),
+            self.__generate_constructor_decl(),
+            self.__generate_fields(),
+            self.__generate_constructor_end(),
+            self.__generate_visitor_method(),
+            self.__generate_final_fields(),
+            self.__generate_class_end()
+        ])
+
+    def __generate_class_decl(self) -> str:
+        return f"  static class {self.name} extends {self.base_name} {{\n"
+
+    def __generate_constructor_decl(self) -> str:
+        params = [str(param) for param in self.parameters]
+        return f"    {self.name}({', '.join(params)}) {{\n"
+
+    def __generate_fields(self) -> str:
+        return "".join(f"        this.{p.param_name} = {p.param_name};\n"
+                       for p in self.parameters)
+
+    def __generate_constructor_end(self) -> str:
+        return "    }\n\n"
+
+    def __generate_visitor_method(self) -> str:
+        override = "    @Override\n"
+        accept = "    <R> R accept(Visitor<R> visitor) {\n"
+        ret = f"        return visitor.visit{self.name}{self.base_name}(this);\n"
+        end = "    }\n\n"
+        return "".join([override, accept, ret, end])
+
+    def __generate_final_fields(self) -> str:
+        return "".join(f"    final {str(p)};\n" for p in self.parameters)
+
+    def __generate_class_end(self) -> str:
+        return "  }\n\n"
+
+
+def build_expressions(base_name: str = "Expression") -> list[AST_Class]:
+    binary = AST_Class(base_name, Expression_Type.BINARY, 
+                    [
+                        Parameter(Parameter_Type.EXPRESSION, "left"),
+                        Parameter(Parameter_Type.TOKEN, "operator"),
+                        Parameter(Parameter_Type.EXPRESSION, "right")
+                    ])
+
+    grouping = AST_Class(base_name, Expression_Type.GROUPING,
+                    [
+                        Parameter(Parameter_Type.EXPRESSION, "expression")
+                    ])
+
+    literal = AST_Class(base_name, Expression_Type.LITERAL,
+                    [
+                        Parameter(Parameter_Type.OBJECT, "value")
+                    ])
+
+    unary = AST_Class(base_name, Expression_Type.UNARY,
+                    [
+                      Parameter(Parameter_Type.TOKEN, "operator"),
+                      Parameter(Parameter_Type.EXPRESSION, "right")
+                    ])
+
+    return [binary, grouping, literal, unary]
+
+
+def build_statements(base_name: str = "Statement") -> list[AST_Class]:
+    ...
+
+
+def generate_visitor_interface(base_name: str, expression_types: list[str]
+                               ) -> str:
+    start = "  interface Visitor<R> {\n"
+    types = ''.join(f"    R visit{name}{base_name}({name} {base_name.casefold()});\n"
+                    for name in expression_types)
+    end = "  }\n\n"
+    return start + types + end
+
+
+def base_accept_method() -> str:
+    comment = "  // Base accept method\n"
+    method = "  abstract <R> R accept(Visitor<R> visitor);\n"
+    return comment + method
+
+
+def define_ast(output_dir: str, base_name: str, ast: list[AST_Class]) -> None:
+    filepath = path.join(output_dir, f"{base_name}.java")
+    enum_type = type(ast[0].name)
+    header = [
+"""// This file was generated using tool/generate_ast.py any changes made
+// here may be overwritten.
+""",
+        "package com.cthuloops.jlox;\n\n",
+        "import java.util.List;\n\n",
+        f"abstract class {base_name} {{\n"
+    ]
+    with open(filepath, "w", encoding="utf8") as f:
+        f.writelines(header)
+        f.write(generate_visitor_interface(base_name,
+                                           [str(name) for name in enum_type]))
+        for item in ast:
+            f.write(str(item))
+
+        f.write(base_accept_method())
+        f.write("}\n")
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: generate_ast <output directory>", file=sys.stderr)
+        exit(64)
+
+    define_ast(sys.argv[1], "Expression", build_expressions())
+    define_ast(sys.argv[1], "Statement", build_statements())
+
+
+if __name__ == "__main__":
+    main()
